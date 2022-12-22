af1195c7ec9fdf02c9eef52734f69d1d276d1b99
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-22 10:32:24 +0530

    😀

--- src/Arrays/SpanOfArray.java
+++ src/Arrays/SpanOfArray.java
@@ -1,6 +1,12 @@
 package Arrays;
 
 public class SpanOfArray {
+
+    public static void swap ( int [] arr , int i , int j ){
+        int temp = arr[i];
+        arr[i ]= arr [j];
+        arr[j] =temp;
+    }
     public static void main(String[] args) {
         int [] arr = new int [5];
         arr[0]= 20;
@@ -9,7 +15,9 @@ public class SpanOfArray {
         arr[3]= 23;
         arr[4]= 24;
 
+        swap(arr,0,4);
         System.out.println(arr.length);
+
 
         for ( int i = 0 ; i<arr.length;i++){
             System.out.println(arr[i]);
