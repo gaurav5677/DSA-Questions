d5af90bb5e7e4314afc22da599ecaf7c4588abe7
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-01-21 15:54:43 +0530

    Method II : Next Greater Element at right

--- src/_2/Stacks/_3_NextGreaterElementToThe_Right.java
+++ src/_2/Stacks/_3_NextGreaterElementToThe_Right.java
@@ -32,21 +32,41 @@ public class _3_NextGreaterElementToThe_Right {
         int[] nge = new int[arr.length];
 
         Stack<Integer> st = new Stack<>();
-        st.push(arr[arr.length - 1]);
-        nge[arr.length - 1] = -1;
-        for (int i = arr.length - 2; i >= 0; i--) {
 
-            while (st.size() > 0 && arr[i] >= st.peek()) {
+        st.push(0);  // only the 0th element    not array(0)
+
+        for( int i = 0  ; i < arr.length ; i++){
+            while(st.size()  > 0 && arr[i] > arr[st.peek()]){ // most imp step arr[i] > arr[st.peek()]
+                int pos = st.peek();
+                nge[pos] = arr[i];
                 st.pop();
             }
-            if (st.size() == 0) {
-                nge[i] = -1;
-            } else {
-                nge[i] = st.peek();
-            }
-            st.push(arr[i]);
 
+            st.push(i);
         }
-        return nge;
+        while(st.size() > 0 ){
+            int pos  = st.peek();
+            nge[pos] = -1 ;
+          st.pop();
+        }
+
+        return  nge ;
+
+//        st.push(arr[arr.length - 1]);
+//        nge[arr.length - 1] = -1;
+//        for (int i = arr.length - 2; i >= 0; i--) {
+//
+//            while (st.size() > 0 && arr[i] >= st.peek()) {
+//                st.pop();
+//            }
+//            if (st.size() == 0) {
+//                nge[i] = -1;
+//            } else {
+//                nge[i] = st.peek();
+//            }
+//            st.push(arr[i]);
+//
+//        }
+//        return nge;
     }
 }
