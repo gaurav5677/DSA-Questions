ae3154395a771f53ebf21a64bdfb04ddb87be87a
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-01-30 17:24:30 +0530

    built Normal Stack

---
+++ src/OOPs/_1_Build_Normal_Stack.java
@@ -0,0 +1,98 @@
+package OOPs;
+
+//import java.util.*;
+import java.io.*;
+
+
+public class _1_Build_Normal_Stack {
+
+    public static class CustomStack {
+        int[] data;
+        int tos; // top of the stack
+
+        public CustomStack(int cap) { // cap = capacity         Constructor
+            data = new int[cap];
+            tos = -1;
+
+        }
+
+        int size() {
+            return tos + 1;
+
+        }
+
+        void display() {
+            for (int i = tos; i >= 0; i--) {
+                System.out.print(data[i] + " ");
+
+            }
+            System.out.println();
+        }
+
+        void push(int val) {
+            if (tos == data.length - 1) {
+                System.out.println("stack overflow");
+            } else {
+                tos++;
+                data[tos] = val;
+            }
+        }
+
+        int pop() {
+            if( tos == -1 ){
+                System.out.println("Stack underflow");
+                return -1;
+            }else{
+                int val = data[tos];
+                tos--;
+                return tos;
+
+            }
+
+        }
+
+        int top() {
+            if(tos == -1 ){
+                System.out.println("Stack underflow");
+                return -1;
+            }else{
+                return data[tos];
+
+            }
+
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        CustomStack st = new CustomStack(n);
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("push")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                st.push(val);
+            } else if (str.startsWith("pop")) {
+                int val = st.pop();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("top")) {
+                int val = st.top();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(st.size());
+            } else if (str.startsWith("display")) {
+                st.display();
+            }
+            str = br.readLine();
+        }
+
+
+    }
+}
