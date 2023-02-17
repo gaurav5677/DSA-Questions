d2d380ad9895cdaba1459178b1a58c47dede5989
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-02-17 09:52:26 +0530

    subset of An Array 🤐🤐

--- src/_01/Arrays/_11_SubSet_Of_an_Array.java
+++ src/_01/Arrays/_11_SubSet_Of_an_Array.java
@@ -1,3 +1,29 @@
+
+//Problem Discussion:
+//        You are given an array of size n, you have to print all subsets/subsequences of the array in separate lines. This problem is also known as: printing power set of an array. You can consider that the array elements will be distinct.
+//
+//        A subsequence/ subset of an array can be formed by choosing some (may be 0, 1, 2, ... or equal to size of array) elements out of all the possible array elements, in the same order in which they appear in the original array.
+//
+//        Example
+//
+//        For Array = {10, 20, 30}, there will be 2n (= 23) Subsets/Subsequences:
+//
+//        - - -
+//
+//        - - 30
+//
+//        - 20 -
+//
+//        - 20 30
+//
+//        10 - -
+//
+//        10 - 30
+//
+//        10 20 -
+//
+//        10 20 30
+
 package _01.Arrays;
 
 import java.util.Scanner;
