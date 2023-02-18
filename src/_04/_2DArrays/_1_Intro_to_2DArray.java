64405d534051819097e0afa9ad6b781c48cec815
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-02-18 17:33:01 +0530

    2d Array into

---
+++ src/_04/_2DArrays/_1_Intro_to_2DArray.java
@@ -0,0 +1,28 @@
+package _04._2DArrays;
+
+import java.util.Scanner;
+
+public class _1_Intro_to_2DArray {
+    public static void main(String[] args) {
+
+        Scanner s  = new Scanner(System.in);
+        int n = s.nextInt();  // rows
+        int m  = s.nextInt(); // cols
+
+        int [][] arr = new int[n][m];
+        for( int i = 0 ; i < n ; i++){
+            for( int j = 0 ; j < m ;j++){
+                arr[i][j] = s.nextInt();
+            }
+        }
+
+        /////////   identify the number of rows       arr.length
+        /////////    identity the number of cols       arr[0].length
+        for(int i = 0 ; i <  arr.length;i++){
+            for( int  j = 0 ; j < arr[i].length ; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
