ac34625fbf8a3215398147fd687a283e89c968b3
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-02-23 12:04:18 +0530

    Rotate anticlockwise

---
+++ src/_04/_2DArrays/_8_Rotate_90deg_AntiClock.java
@@ -0,0 +1,62 @@
+package _04._2DArrays;
+
+import java.util.Scanner;
+
+public class _8_Rotate_90deg_AntiClock {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int[][] arr = new int[n][n];
+        for( int i = 0 ; i < arr.length;i++){
+            for( int j = 0 ; j< arr[0].length;j++){
+                arr[i][j] = s.nextInt();
+            }
+        }
+
+        //Transpose
+
+        /// this will wont work buc swaping happeans 2times on every eleement , resulting origial matrix
+//        for( int i  = 0 ; i < arr.length ; i++){
+//            for( int j = 0 ;  j <arr[0].length ; j++){
+//                int temp = arr[i][j];
+//                arr[i][j]=arr[j][i];
+//                arr[j][i] = temp;
+//            }
+//        }
+        //Transpose
+      for( int i = 0 ; i < arr.length; i++){
+          for( int j = i ; j < arr[0].length ; j++){ //loop will run in  upper trangle
+              int temp =arr[i][j];
+              arr[i][j]=arr[j][i];
+              arr[j][i] = temp;
+          }
+      }
+
+
+      for( int i = 0 ; i < arr.length ; i++){
+          int li = 0 ;
+            int ri = arr[i].length-1;
+
+            while( li < ri){
+                int temp = arr[li][i];
+                arr[li][i] = arr[ri][i];
+                arr[ri][i]=temp;
+                li++;
+                ri--;
+            }
+      }
+
+      display(arr);
+
+    }
+
+    public static void display( int[] [] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+            }
+        }
+    }
