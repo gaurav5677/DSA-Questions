0587666602c04b45afd89772e1c8232ebd254aeb
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-02-23 10:32:16 +0530

    Exit point 😵

--- src/_04/_2DArrays/_7_Exit_point.java
+++ src/_04/_2DArrays/_7_Exit_point.java
@@ -4,52 +4,50 @@ import java.util.Scanner;
 
 public class _7_Exit_point {
     public static void main(String[] args) {
-        Scanner s = new Scanner( System.in);
-        int n  = s.nextInt();
-        int m  = s.nextInt();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
 
-        int [] [] arr = new int[n][m];
+        int[][] arr = new int[n][m];
 
-        for( int i = 0 ; i < arr.length ; i++){
-            for( int j = 0 ; j < arr[0].length;j++){
-                arr[i][j]=s.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = s.nextInt();
             }
         }
 
-         int dir  = 0 ;     //0 == east , 1 = south , 2 = west , 3 = north
-         int i = 0 ; // entry point;
-         int j = 0 ; // enty point;
-
-        while(true){
-               dir = ( dir + arr[i][j]) %4 ;
-            if(dir ==  0 ){ // East
-                j++;
-            }else if( dir ==1  ){ // South
-                i++;
-            }else if (dir ==2 ){ // west
-                j--;
-            }else if (dir==3  ){ // North
-                i--;
+        int i = 0;
+        int j = 0;
+        int dir = 0;   //East->0 South->1 West->2 North->3
+        while (true) {
+            dir = (dir + arr[i][j]) % 4; //To Keep the values between 0 and 3
+            if (dir == 0) {
+                j++;  //Move in the same row (east direction)
+            } else if (dir == 1) {
+                i++;  //Move down the column (south direction)
+            } else if (dir == 2) {
+                j--;  //Move backwards in the row (west direction)
+            } else if (dir == 3) {
+                i--;  //Move up the column (north direction)
             }
 
-            if( i < 0 ){
-                i++;
+            if (i < 0) {
+                i++;  //exit point is one row head (in 0th row)
                 break;
-
-            }else if (j < 0){
-                j++;
+            } else if (j < 0) {
+                j++;   //exit point is one col ahead (in 0th col)
                 break;
-            }
-            else if ( i ==arr.length){
-                i--;
+            } else if (i == n) {
+                i--;  //exit point is one row back (in last row)
                 break;
-            }else if ( j == arr[0].length){
-                j--;
+            } else if (j == m) {
+                j--;  //exit point is one col back (in last col)
                 break;
             }
         }
+
         System.out.println(i);
         System.out.println(j);
-    }
 
+    }
 }
