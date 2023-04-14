093694cb256b40cdc74a7566261be12e1227fd92
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-04-14 10:32:46 +0530

    Clockwise Spiral traversal

--- src/_04/_2DArrays/_05_Spiral_Traversal_clockwise.java
+++ src/_04/_2DArrays/_05_Spiral_Traversal_clockwise.java
@@ -28,14 +28,16 @@ public class _05_Spiral_Traversal_clockwise {
         //
         while(count < te){
             //top wall
-            for( int i = minr , j = minc ; j <= maxc && count < te ; j++){
+            for( int i = minr , j = minc ; j <= maxc && count < te ; j++){// count < tn is added here , cause not every element will surround by walls, all the time ; this
+                // will print only the last remaining element
 //                ans.add(matrix[i][j]);
                 System.out.println(arr[i][j]);
                 count++;
             }
             minr++;
             //right wall
-            for(int i = minr  , j = maxc; i <=maxr && count < te;i++ ){
+            for(int i = minr  , j = maxc; i <=maxr && count < te;i++ ){// count < tn is added here , cause not every element will surround by walls, all the time ; this
+                // will print only the last remaining element
 //                ans.add(matrix[i][j]);
                 System.out.println(arr[i][j]);
 
@@ -43,7 +45,8 @@ public class _05_Spiral_Traversal_clockwise {
             }
             maxc--;
             //bottom wall
-            for( int i = maxr , j = maxc ; j >= minc && count < te ; j-- ){
+            for( int i = maxr , j = maxc ; j >= minc && count < te ; j-- ){// count < tn is added here , cause not every element will surround by walls, all the time ; this
+                // will print only the last remaining element
 //                ans.add(matrix[i][j]);
                 System.out.println(arr[i][j]);
                 count++;
@@ -51,7 +54,8 @@ public class _05_Spiral_Traversal_clockwise {
             maxr--;
 
             //left wall
-            for( int i = maxr  , j = minc ; i  >=minr && count < te ; i--){
+            for( int i = maxr  , j = minc ; i  >=minr && count < te ; i--){// count < tn is added here , cause not every element will surround by walls, all the time ; this
+                // will print only the last remaining element
 //                ans.add(matrix[i][j]);
                 System.out.println(arr[i][j]);
                 count++;
