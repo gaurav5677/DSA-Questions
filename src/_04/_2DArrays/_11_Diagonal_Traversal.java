af467748e1bc8b534d22c1cbae4451364608503a
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-04-15 11:05:42 +0530

    Diagonal Traversal Upward 📈

--- src/_04/_2DArrays/_11_Diagonal_Traversal.java
+++ src/_04/_2DArrays/_11_Diagonal_Traversal.java
@@ -20,7 +20,7 @@ public class _11_Diagonal_Traversal {
 
         // Diagonal Traversal  upward  from the Main diagonal  ( i = 0 , j = 0 )
         for( int g = 0 ; g < arr.length ; g++){
-            for( int i = g , j = 0 ; i <arr.length ; i++ , j++){
+            for( int i =  0, j = g ; j <arr.length ; i++ , j++){
                 System.out.println(arr[i][j]);
             }
         }
